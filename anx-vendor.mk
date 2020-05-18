fbd6329b32f8b1adac19f8e17c97d7d1b6d927ce
sarveshrulz <sarveshkardekar@gmail.com>
2020-05-18 08:24:29 +0530

    ANXCamera: add init service

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -2,6 +2,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/aeonax/ANXCamera
 
 PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXCamera/lib)
 
